a2d790cf601494716513a1d6a5740f0e13d5ed53
tiago <tiago.astro@gmail.com>
2016-08-07 08:26:12 -0300

    There was a strange behavior with the ceil function on dome sync. Trying to fix it.

--- chimera_astelco/instruments/astelcodome.py
+++ chimera_astelco/instruments/astelcodome.py
@@ -191,7 +191,7 @@ class AstelcoDome(DomeBase):
                 if time.time() > (start_time + self._maxSlewTime):
                     self.syncComplete()
                     raise AstelcoDomeException("Dome synchronization timed-out")
-                elif np.ceil(abs(caz - target_az)) < tpl.getobject('POINTING.SETUP.DOME.MAX_DEVIATION') * 2.0:
+                elif abs(caz - target_az)+0.5 < tpl.getobject('POINTING.SETUP.DOME.MAX_DEVIATION') * 2.0:
                     break
 
 
